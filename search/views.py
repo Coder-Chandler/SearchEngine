4a5c66adfcb31bf32dea0f17e3ed80ca4370ce00
Coder-Chandler <ysiwgtus@gmail.com>
2017-07-07 23:05:12 +0800

    SPIDER AND SEARCH ENGINE

--- search/views.py
+++ search/views.py
@@ -49,7 +49,8 @@ class SearchSuggest(View):
         return HttpResponse(json.dumps(re_datas), content_type="application/json")
 
 
-def SearchResult(tag, key_words, page):
+def client_search(tag, key_words, page):
+    # 代码复用， 定义搜索项目
     response = client.search(
         index=tag,
         body={
@@ -63,7 +64,7 @@ def SearchResult(tag, key_words, page):
             "size": 10,
             "highlight": {
                 "pre_tags": ['<span class="keyWord">'],
-                "post_tags": ['</span class="keyWord">'],
+                "post_tags": ['</span>'],
                 "fields": {
                     "title": {},
                     "content": {},
@@ -74,9 +75,60 @@ def SearchResult(tag, key_words, page):
     return response
 
 
+def get_hit_dict(click_words, hit_dict, hit, content, create_date):
+    # 代码复用，把数据传进client_search
+    hit_dict["from"] = click_words
+    if "title" in hit["highlight"]:
+        hit_dict["title"] = "".join(hit["highlight"]["title"])
+    else:
+        hit_dict["title"] = hit["_source"]["title"]
+    if "content" in hit["highlight"]:
+        hit_dict["content"] = "".join(hit["highlight"][content])[:500]
+    else:
+        hit_dict["content"] = hit["_source"][content][:500]
+    hit_dict["create_date"] = hit["_source"][create_date]
+    hit_dict["tags"] = hit["_source"]["tags"]
+    hit_dict["url"] = hit["_source"]["url"]
+    hit_dict["score"] = hit["_score"]
+    return hit_dict
+
+
+# class Detail(View):
+def SearchResult(key_words, page, click_words):
+    # 单独定义获取搜索页面所需要的数据，以便代码复用
+    if click_words == "article":
+        tag = "jobbole"
+        response = client_search(tag, key_words, page)
+        return response
+    if click_words == "job":
+        tag = "lagou"
+        response = client_search(tag, key_words, page)
+        return response
+
+
+def GetTagDetail(response, click_words):
+    hit_list = []
+    for hit in response["hits"]["hits"]:
+        hit_dict = {}
+        if click_words == "article":
+            name = "伯乐在线"
+            time = "create_date"
+            content = "content"
+            hit_dict = get_hit_dict(name, hit_dict, hit, content, time)
+        hit_list.append(hit_dict)
+        if click_words == "job":
+            name = "拉勾网"
+            time = "publish_time"
+            content = "job_desc"
+            hit_dict = get_hit_dict(name, hit_dict, hit, content, time)
+        hit_list.append(hit_dict)
+    return hit_list
+
+
 class SearchView(View):
     def get(self, request):
         key_words = request.GET.get("q", "")
+        click_words = request.GET.get('s_type', '')
         s_type = request.GET.get("s_type", "article")
         redis_cli.zincrby("search_keywords_set", key_words)
 
@@ -89,31 +141,15 @@ class SearchView(View):
         jobbole_count = redis_cli.get("jobbole_count")
         lagoujob_count = redis_cli.get("lagoujob_count")
         start_time = datetime.now()
-        response = SearchResult("jobbole", key_words, page)
+        response = SearchResult(key_words, page, click_words)
         end_time = datetime.now()
         last_seconds = (end_time - start_time).total_seconds()
         total_nums = response["hits"]["total"]
         if (page % 10) > 0:
-            page_nums = int(total_nums/10) + 1
+            page_nums = int(total_nums / 10) + 1
         else:
-            page_nums = int(total_nums/10)
-        hit_list = []
-        for hit in response["hits"]["hits"]:
-            hit_dict = {}
-            if "title" in hit["highlight"]:
-                hit_dict["title"] = "".join(hit["highlight"]["title"])
-            else:
-                hit_dict["title"] = hit["_source"]["title"]
-            if "content" in hit["highlight"]:
-                hit_dict["content"] = "".join(hit["highlight"]["content"])[:500]
-            else:
-                hit_dict["content"] = hit["_source"]["content"][:500]
-
-            hit_dict["create_date"] = hit["_source"]["create_date"]
-            hit_dict["url"] = hit["_source"]["url"]
-            hit_dict["score"] = hit["_score"]
-
-            hit_list.append(hit_dict)
+            page_nums = int(total_nums / 10)
+        hit_list = GetTagDetail(response, click_words)
         return render(request, "result.html", {"page": page,
                                                "s_type": s_type,
                                                "all_hits": hit_list,
